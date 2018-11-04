362003b9ab4121cfd71c2de0f986137aac068417
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-04 23:03:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    counter -= 1 
+  end 
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    counter -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end 
